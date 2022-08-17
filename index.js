e0eaa97eaaf68f38aac8d28a3152dad5076d8bc6
Rohit Bahuguna <90442893+rohit-bahuguna@users.noreply.github.com>
2022-08-17 08:00:26 +0000

    ok

--- index.js
+++ index.js
@@ -3,25 +3,40 @@
 
 // init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+// so that API is remotely testable by FCC 
+const cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
+// to serve the static CSS file from the public folder by using the 
+// built-in middleware function in Express
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
+// routing--how the app responds to a client request to a particular endpoint
+// when the route is matched, the handler function is executed--responds with the index.html file
+app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+
+// first test API endpoint... 
+app.get("/api/hello", (req, res) => {
+  res.json({greeting: 'hello API'});
+});
+
+// requested API endpoint from user stories--get the IP address, preferred languages, and system infos from the header
+app.get("/api/whoami", (req, res) => {
+  let myIP = req.header("X-Forwarded-For").split(',')[0];  // X-Forwarded-For method gets three IP addresses--client, proxy1, proxy 2
+  let myLanguage = req.header('Accept-Language');
+  let mySystem = req.header('User-Agent');
+  res.json({
+    ipaddress: myIP,
+    language: myLanguage,
+    software: mySystem
+  });
 });
 
 // listen for requests :)
